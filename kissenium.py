fb0275482515d3e7a19c2934e624bb04dd8d4bf3
nboudou <n.boudou@adiuvo.fr>
2017-09-15 14:50:29 +0200

    Adding licence to file and first kissenium template. Another template must follow in a few times.

--- kissenium.py
+++ kissenium.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 # coding: utf-8
+
+"""
+Copyright 2017 Adiuvo
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import glob
 import sys
 from unittest import TestLoader, TestSuite
@@ -36,7 +53,7 @@ class Runner:
             self.suites.append(suite)
 
         suite = TestSuite(self.suites)
-        test_runner = HTMLTestRunner(output='html', report_title='Kissenium')
+        test_runner = HTMLTestRunner(output='html', template='resources/html/report-template.html', report_title='Test report')
         results = test_runner.run(suite)
         self.logger.info("All tests have been executed. Kissenium will stop now.")
         sys.exit(not results.wasSuccessful())
