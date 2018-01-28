55efea9867e291b6db2ea32131f7d5fa836e67c8
Nathan Boudou <n.boudou@adiuvo.fr>
2018-01-28 11:16:14 +0100

    First session on work from pytlint, I need to create a pylint file to have kissenium rules

--- base/reports/decorators.py
+++ base/reports/decorators.py
@@ -4,9 +4,7 @@
 def exception(message=None):
     """
     A decorator that wraps the passed in function and logs
-    exceptions should one occur
-
-    @param message: The message to print
+    @param message: The message to print (optionnal)
     """
     def decorator(func):
         def wrapper(self, *args, **kwargs):
@@ -17,9 +15,9 @@ def exception(message=None):
                     self.logger.error(message)
                 self.logger.error('[%s] Error is : %s' %(func.__name__, e))
                 if self.config.get_capture_on_fail() == "True":
-                    if not 'browser' in dir(self) and not 'screenshot' in dir(self) :
+                    if not 'browser' in dir(self) and not 'screenshot' in dir(self):
                         self.logger.error('Can\'t take a screenshot, either <browser> or <screenshot> is <None>')
-                    else :
+                    else:
                         self.screenshot.capture(self.browser, message)
                 if self.config.get_fail_on_error() == "True":
                     raise Exception(message)
@@ -32,8 +30,6 @@ def exception(message=None):
 def assertion_error():
     """
     A decorator to use in comparing assert
-
-    @param operator: The operator used for the assert
     """
     def decorator(func):
         def wrapper(self, *args, **kwargs):
